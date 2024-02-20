21b353fbcb1c06a4485b60ace72c2b8f54f68334
HyungJoo <prism9411@gmail.com>
2024-02-20 14:52:42 +0900

    기초

--- HelloSDK/HelloSDK/HelloSDK.cpp
+++ HelloSDK/HelloSDK/HelloSDK.cpp
@@ -1,8 +1,83 @@
 #include <Windows.h>
 
+LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
+
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
 {
+	WNDCLASS wndclass;
+	HWND hwnd; 
+	MSG msg;
 
+	//1. Initialize Window Class  And Regist To OS
+	wndclass.style = CS_HREDRAW | CS_VREDRAW;
+	wndclass.lpfnWndProc = WndProc;
+	wndclass.cbClsExtra = 0;
+	wndclass.cbWndExtra = 0;
+	wndclass.hInstance = hInstance;
+	wndclass.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+	wndclass.hCursor = LoadCursor(NULL, IDC_ARROW);
+	wndclass.hbrBackground = (HBRUSH)GetStockObject(WHITE_BRUSH);
+	wndclass.lpszMenuName = NULL;
+	wndclass.lpszClassName = TEXT("Hello Class");
+
+
+	if (!RegisterClass(&wndclass))
+		return 1;
+
+	//2. Create Window & Renderer
+	hwnd = CreateWindow(
+		TEXT("Hello Class"), 
+		TEXT("HelloSDK"),
+		WS_OVERLAPPEDWINDOW,
+		CW_USEDEFAULT,
+		CW_USEDEFAULT,
+		CW_USEDEFAULT,
+		CW_USEDEFAULT,
+		NULL,
+		NULL,
+		hInstance,
+		NULL
+	);
+
+	ShowWindow(hwnd, nCmdShow);
+
+
+	//3. Message Queue 
+	while (GetMessage(&msg, NULL, 0, 0) > 0)
+	{
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+	}
 
 	return 0;
+}
+
+//Window Procedure
+LRESULT CALLBACK WndProc(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+	HDC hdc;
+	PAINTSTRUCT ps;
+	const TCHAR *str = TEXT("Hello, SDK");
+
+	//Handle By Event Type
+	switch (message)
+	{
+	case WM_CREATE:
+		return 0;
+
+	case WM_LBUTTONDOWN:
+		MessageBox(hwnd, TEXT("Click!"), TEXT("Click message"), MB_OK);
+		return 0;
+	case WM_PAINT:
+		hdc = BeginPaint(hwnd, &ps);
+		TextOut(hdc, 100, 100, str, lstrlen(str));
+		EndPaint(hwnd, &ps);
+		return 0;
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		return 0;
+	}
+
+	//Unhandled Message
+	return DefWindowProc(hwnd, message, wParam, lParam);
 }
