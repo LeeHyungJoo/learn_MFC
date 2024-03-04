b98d12e96a6d6c64c62fd9f03479aac41171ef42
Yodle_94 <sj_artist@naver.com>
2024-03-04 21:15:00 +0900

    소수점, 분수 표현 BOOL 값 추가.

--- AaronMathViewer/AaronMathViewer/Formatter.cpp
+++ AaronMathViewer/AaronMathViewer/Formatter.cpp
@@ -2,78 +2,152 @@
 #include "pch.h"
 #include "Formatter.h"
 
+static bool DECIMAL_FORMAT = TRUE;
+
 void Formatter::LineQuation(const CString & desc, const RationalNum & gradient, const RationalNum & constant, OUT CString * format)
 {
-	if (gradient.IsInteger())
+	if (DECIMAL_FORMAT)
 	{
-		if (constant.IsInteger())
-			format->Format(_T("%s [ y = %+.0fx %+.0f ]"),
-				desc,
-				gradient.GetNumerator(),
-				constant.GetNumerator()
-			);
+		if (gradient.IsInteger())
+		{
+			if (constant.IsInteger())
+				format->Format(_T("%s [ y = %+.0fx %+.0f ]"),
+					desc,
+					gradient.GetNumerator(),
+					constant.GetNumerator()
+				);
+			else
+				format->Format(_T("%s [ y = %+.0fx %+.3f ]"),
+					desc,
+					gradient.GetNumerator(),
+					constant.GetNumerator() / constant.GetDenomiator()
+				);
+		}
 		else
-			format->Format(_T("%s [ y = %+.0fx %+.0f/%.0f ]"),
-				desc,
-				gradient.GetNumerator(),
-				constant.GetNumerator(),
-				constant.GetDenomiator()
-			);
+		{
+			if (constant.IsInteger())
+				format->Format(_T("%s [ y = %+.3fx %+.0f ]"),
+					desc,
+					gradient.GetNumerator() / gradient.GetDenomiator(),
+					constant.GetNumerator()
+				);
+			else
+				format->Format(_T("%s [ y = %+.3fx %+.3f ]"),
+					desc,
+					gradient.GetNumerator() / gradient.GetDenomiator(),
+					constant.GetNumerator() / constant.GetDenomiator()
+				);
+		}
 	}
 	else
 	{
-		if (constant.IsInteger())
-			format->Format(_T("%s [ y = %+.0f/%.0fx %+.0f ]"),
-				desc,
-				gradient.GetNumerator(),
-				gradient.GetDenomiator(),
-				constant.GetNumerator()
-			);
+		if (gradient.IsInteger())
+		{
+			if (constant.IsInteger())
+				format->Format(_T("%s [ y = %+.0fx %+.0f ]"),
+					desc,
+					gradient.GetNumerator(),
+					constant.GetNumerator()
+				);
+			else
+				format->Format(_T("%s [ y = %+.0fx %+.0f/%.0f ]"),
+					desc,
+					gradient.GetNumerator(),
+					constant.GetNumerator(),
+					constant.GetDenomiator()
+				);
+		}
 		else
-			format->Format(_T("%s [ y = %+.0f/%.0fx %+.0f/%.0f ]"),
-				desc,
-				gradient.GetNumerator(),
-				gradient.GetDenomiator(),
-				constant.GetNumerator(),
-				constant.GetDenomiator()
-			);
+		{
+			if (constant.IsInteger())
+				format->Format(_T("%s [ y = %+.0f/%.0fx %+.0f ]"),
+					desc,
+					gradient.GetNumerator(),
+					gradient.GetDenomiator(),
+					constant.GetNumerator()
+				);
+			else
+				format->Format(_T("%s [ y = %+.0f/%.0fx %+.0f/%.0f ]"),
+					desc,
+					gradient.GetNumerator(),
+					gradient.GetDenomiator(),
+					constant.GetNumerator(),
+					constant.GetDenomiator()
+				);
+		}
 	}
 }
 
 void Formatter::Coord(const CString & desc, const RationalNum & x, const RationalNum & y, OUT CString * format)
 {
-	if (x.IsInteger())
+	if (DECIMAL_FORMAT)
 	{
-		if (y.IsInteger())
-			format->Format(_T("%s ( %+.0f, %.0f )"),
-				desc,
-				x.GetNumerator(),
-				y.GetNumerator()
-			);
+		if (x.IsInteger())
+		{
+			if (y.IsInteger())
+				format->Format(_T("%s ( %+.0f, %.0f )"),
+					desc,
+					x.GetNumerator(),
+					y.GetNumerator()
+				);
+			else
+				format->Format(_T("%s ( %+.0f, %+.3f )"),
+					desc,
+					x.GetNumerator(),
+					y.GetNumerator() / y.GetDenomiator()
+				);
+		}
 		else
-			format->Format(_T("%s ( %+.0f, %+.0f/%.0f )"),
-				desc,
-				x.GetNumerator(),
-				y.GetNumerator(),
-				y.GetDenomiator()
-			);
+		{
+			if (y.IsInteger())
+				format->Format(_T("%s ( %+.3f, %.0f )"),
+					desc,
+					x.GetNumerator() / x.GetDenomiator(),
+					y.GetNumerator()
+				);
+			else
+				format->Format(_T("%s ( %+.3f, %+.3f )"),
+					desc,
+					x.GetNumerator() / x.GetDenomiator(),
+					y.GetNumerator() / y.GetDenomiator()
+				);
+		}
 	}
 	else
 	{
-		if (y.IsInteger())
-			format->Format(_T("%s ( %+.0f/%.0f, %.0f )"),
-				desc,
-				x.GetNumerator(),
-				x.GetDenomiator(),
-				y.GetNumerator()
-			);
+		if (x.IsInteger())
+		{
+			if (y.IsInteger())
+				format->Format(_T("%s ( %+.0f, %.0f )"),
+					desc,
+					x.GetNumerator(),
+					y.GetNumerator()
+				);
+			else
+				format->Format(_T("%s ( %+.0f, %+.0f/%.0f )"),
+					desc,
+					x.GetNumerator(),
+					y.GetNumerator(),
+					y.GetDenomiator()
+				);
+		}
 		else
-			format->Format(_T("%s ( %+.0f/%.0f, %+.0f/%.0f )"),
-				desc,
-				x.GetNumerator(),
-				x.GetDenomiator(),
-				y.GetNumerator(),
-				y.GetDenomiator()
-			);
+		{
+			if (y.IsInteger())
+				format->Format(_T("%s ( %+.0f/%.0f, %.0f )"),
+					desc,
+					x.GetNumerator(),
+					x.GetDenomiator(),
+					y.GetNumerator()
+				);
+			else
+				format->Format(_T("%s ( %+.0f/%.0f, %+.0f/%.0f )"),
+					desc,
+					x.GetNumerator(),
+					x.GetDenomiator(),
+					y.GetNumerator(),
+					y.GetDenomiator()
+				);
+		}
 	}
 }
